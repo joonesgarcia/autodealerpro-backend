990e06691b148eb2acc0df8b95be3f1861eda31d
joaog <joao.garcia.vitor@hotmail.com>
2026-04-24 18:41:53 -0300

    adding missing services/validators for vehicle module

--- src/Modules/Inventory/AutoDealerPro.Modules.Inventory.Infrastructure/InventoryModule.cs
+++ src/Modules/Inventory/AutoDealerPro.Modules.Inventory.Infrastructure/InventoryModule.cs
@@ -1,7 +1,12 @@
-﻿using AutoDealerPro.Modules.Inventory.Core.Repositories;
+﻿using AutoDealerPro.Modules.Inventory.Application.Interfaces;
+using AutoDealerPro.Modules.Inventory.Application.Requests;
+using AutoDealerPro.Modules.Inventory.Application.Services;
+using AutoDealerPro.Modules.Inventory.Application.Validators;
+using AutoDealerPro.Modules.Inventory.Core.Repositories;
 using AutoDealerPro.Modules.Inventory.Infrastructure.Persistence;
 using AutoDealerPro.Modules.Inventory.Infrastructure.Repositories;
 using AutoDealerPro.Shared.Abstractions.Modules;
+using FluentValidation;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,6 +24,16 @@ public class InventoryModule : IModule
                 b => b.MigrationsHistoryTable("__EFMigrationsHistory", "inventory")));
 
         services.AddScoped<IVehicleRepository, VehicleRepository>();
+
+        services.AddScoped<IInventoryService, InventoryService>();
+
+        // Validators
+        services.AddScoped<IValidator<AddPhotoRequest>, AddPhotoValidator>();
+        services.AddScoped<IValidator<CreateVehicleRequest>, CreateVehicleValidator>();
+        services.AddScoped<IValidator<MarkAsSoldRequest>, MarkAsSoldValidator>();
+        services.AddScoped<IValidator<UpdateMileageRequest>, UpdateMileageValidator>();
+        services.AddScoped<IValidator<UpdatePriceRequest>, UpdatePriceValidator>();
+
     }
 
     public void MapEndpoints(IEndpointRouteBuilder endpoints)
